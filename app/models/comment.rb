f15a4d2721c32d09c0dcb2aa71f1dd97dac49439
Your Name <phagmann1@gmail.com>
2016-09-13 22:50:33 +0000

    Cleaner

---
+++ app/models/comment.rb
@@ -0,0 +1,16 @@
+class Comment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :place
+
+  RATINGS = {
+    '★'    => '1_star',
+    '★★'   => '2_stars',
+    '★★★' => '3_stars',
+    '★★★★'  => '4_stars',
+    '★★★★★'  => '5_stars'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
+end
