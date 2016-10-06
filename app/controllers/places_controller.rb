d3b96e6e2874338ae68de3b3e012b05ff107f207
Patrick Hagmann <phagmann1@gmail.com>
2016-10-06 23:39:35 +0000

    fixes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,9 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
-
+  
   def create
+   
      @place = current_user.places.create(place_params)
     if @place.valid?
      redirect_to root_path
